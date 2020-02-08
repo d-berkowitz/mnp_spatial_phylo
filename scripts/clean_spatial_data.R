f1d9977e7f5774a8fb29c37a35c487d43ebc7a18
Jenna Ekwealor <jtbe@berkeley.edu>
2020-02-07 16:32:40 -0800

    updated script to include load csv and beginnings of merge functions

--- scripts/clean_spatial_data.R
+++ scripts/clean_spatial_data.R
@@ -1,3 +1,20 @@
 library(tidyverse)
+library(dplyr)
+library(readr)
 
-setwd("/Users/jennaekwealor/Documents/dean_project/mnp_spatial_phylo") # change to your directory
+setwd("/Users/jennaekwealor/Documents/dean_project/mnp_spatial_phylo") # change to your project HOME directory directory
+
+# load all csv files and merge into one large data table
+path <- "data/raw/spatial" # path to your raw data, if different organization than this
+
+files <- list.files(path = path, full.names = T)
+tbl <- sapply(files, read_csv, simplify=FALSE) %>% 
+ bind_rows(.id = "id")
+
+# remove any fully duplicated rows
+data <- distinct(data)
+
+# pull out columns we're interested in
+spatial_data <- data[]
+
+# 
