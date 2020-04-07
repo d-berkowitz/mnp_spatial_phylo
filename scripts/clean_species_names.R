0dd01e56272b644fd06ddef2d87a4f1f6af91502
Dean Berkowitz <dberkowitz@berkeley.edu>
2020-04-07 10:22:37 -0700

    tidied up script, removed section that cleans spatial data and moved to its own script

--- scripts/clean_species_names.R
+++ scripts/clean_species_names.R
@@ -1,33 +1,29 @@
-#clean_species_names script, based on Viktoria Wagner's blog post on how to use the taxize package
+#clean_species_names script, based on Viktoria Wagner's blog post on how to use the taxize package.
+#automate cleaning of erroneous taxon names by comparing taxa to reference databases.
 
 #load libraries
 library(dplyr)
 library(taxize)
 library(magrittr)
 library(tidyverse)
-library(DataCombine)
-library(comprehenr)
 library(sf)
 
 # change to your project HOME directory
 setwd("/Users/deanberkowitz/Documents/mishler_lab/thesis/mnp_spatial_phylo/") 
 
 # set filepath
-path <- "data/semiclean" # path to your data
-
-#list files
-files <- list.files(path = path, full.names = T)
+path <- "data/semiclean/spatial_data.csv" # path to your data
 
 #read in data
-my_data <- read.csv(files[1])
-head(my_data)
+my_data <- read.csv(file = "data/semiclean/spatial_data.csv")
 
 #replace '#N/A' with NA across dataframe
 my_data <- as.data.frame(sapply(my_data, function(x) {
   gsub('#N/A', NA, x)
 }))
 
-#split data into 2 dataframes in order to move observations from the columns genus, species to the column Genus_Species
+#split data into 2 dataframes in order to move observations from the columns genus, 
+#species to the column Genus_Species
 
 na_gensp <- my_data %>%
   filter(is.na(Genus_Species))
@@ -35,7 +31,8 @@ na_gensp <- my_data %>%
 not_na_gensp <- my_data %>%
   filter(!is.na(Genus_Species))
 
-#move all values from separate genus, species columns to the Genus_Species column to prepare for taxa name spellchecking/cleaning
+#move all values from separate genus, species columns to the Genus_Species column 
+#to prepare for taxa name spellchecking/cleaning
 
 na_gensp$Genus_Species <- paste(na_gensp$genus, na_gensp$species)
 na_gensp
@@ -106,186 +103,30 @@ dupl_remov <- dupl_remov %>%
   tidyr::replace_na(list(Genus_species = 'Unknown', Family = 'Unknown'))
 
 #remove observations where both taxa and family are unknown
-cleaned_data <- dupl_remov[!(dupl_remov$Genus_species == 'Unknown' & dupl_remov$Family =='Unknown'),]
+cleaned_sp_names <- dupl_remov[!(dupl_remov$Genus_species == 'Unknown' & dupl_remov$Family =='Unknown'),]
 
 #IDIOSYNCRATIC STEP, UNECCESSARY TO REPLICATE. replace all occurrences of 'Cylindropuntia bigelovii' with 
 #'Cylindropuntia echinocarpa' and also 'Cylindropuntia californica' to 'Cylindropuntia acanthocarpa' 
 #'to rectify plant misidentification from data collection process
-cleaned_data$Genus_species <- sapply(cleaned_data$Genus_species, function(taxon) {
+cleaned_sp_names$Genus_species <- sapply(cleaned_sp_names$Genus_species, function(taxon) {
   gsub("Cylindropuntia bigelovii", "Cylindropuntia echinocarpa", taxon)
 })
 
-cleaned_data$Genus_species <- sapply(cleaned_data$Genus_species, function(taxon) {
+cleaned_sp_names$Genus_species <- sapply(cleaned_sp_names$Genus_species, function(taxon) {
   gsub("Cylindropuntia californica", "Cylindropuntia acanthocarpa", taxon)
 })
+
+#write cleaned_species_names to .csv
+write.csv(cleaned_sp_names, file = "data/semiclean/clean_species_names.csv", row.names = FALSE)
 
 ###########STILL NEED TO REMOVE ALL SINGLE WORD TAXA FROM DATA (EXCEPT PECTOCARYA & UNKNOWN)
 
 
 #extract unique taxa names for use in Genbank query
 taxa_list <- cleaned_data$Genus_species %>% sort() %>% unique()
-
-
+taxa_list_df <- data.frame(taxa = taxa_list)
 #write taxa list to CSV, .txt files
-write.csv(taxa_list, file = "data/clean/taxa_list.csv") # change path relevant to your directory organization
-write.table(taxa_list, file = "data/clean/taxa_list.txt", sep = " ", col.names = FALSE)
-
-
-###########CLEAN SPATIAL DATA START
-#drop columns in Easting, Northing due to uncertainty about their origins. Will create Easting, Northing from Latitude and Longitude for all data points
-#to minimize error / assumptions about data since some have Easting/Northing and some don't.
-lat_long_only <- cleaned_data %>% select(-c(Easting, Northing))
-
-#create new df with lat, long in dms only to clean and prepare for conversion to dd
-dd_only <- lat_long_only %>% 
-  filter(!grepl(pattern = c(' '), Latitude))
-
-dms_only <- lat_long_only %>% 
-  filter(grepl(pattern = c(' '), Latitude))
-
-# remove ° symbol from longitudes (idiosyncracy of the data I am working with)
-dd_only$Longitude <- dd_only$Longitude %>%
-  gsub(pattern = '°', replacement = '')
-#convert latitude, longitude columns from factor to double in preparation for creating northing, easting
-dd_only$Latitude <- as.numeric(as.character(dd_only$Latitude))
-dd_only$Longitude <- as.numeric(dd_only$Longitude)
-
-#remove rows where latitude == longitude
-dd_only <- dd_only %>%
-  filter(!dd_only$Longitude == dd_only$Latitude)
-
-
-#convert DMS to DD 
-#convert latitude, longitude columns to character vectors to prepare for conversion to decimal degrees
-dms_only$Latitude <- lapply(dms_only$Latitude, as.character)
-dms_only$Longitude <- lapply(dms_only$Longitude, as.character)
-
-# convert degree, minute, second characters in Latitude and Longitude column to 
-#format required by char2dms function
-
-#helper function to fix degree symbol
-fix_deg <- function(degree_symbol) {
-  gsub(pattern = "° ", replacement = "d", degree_symbol)
-}
-
-#helper function to remove spaces
-fix_space <- function(space_symbol) {
-  gsub(pattern = " ", replacement = "", space_symbol)
-}
-
-#helper function to insert explicit N indicating latitude
-fix_north <- function(north_symbol) {
-  gsub(pattern = "\"", replacement = "\"N", north_symbol)
-}
-
-#helper function to insert explicit W indicating longitude
-fix_west <- function(west_symbol) {
-  gsub(pattern = "\"", replacement = "\"W", west_symbol)
-}
-
-#function to convert latitude coordinates to decimal degrees
-fix_latitude <- function(lat_coord){
-  lat_coord1 <- fix_deg(lat_coord)
-  lat_coord2 <- fix_space(lat_coord1)
-  lat_coord3 <- fix_north(lat_coord2)
-  dd_lat <- lat_coord3 %>% sp::char2dms() %>% as.numeric()
-  dd_lat
-}
-
-#function to convert longitude coordinates to decimal degrees
-fix_longitude <- function(long_coord) {
-  long_coord1 <- fix_deg(long_coord)
-  long_coord2 <- fix_space(long_coord1)
-  long_coord3 <- fix_west(long_coord2)
-  dd_long <- long_coord3 %>% sp::char2dms() %>% as.numeric()
-  dd_long
-}
-
-#create test longitude vector to check if function works
-test_long <- dms_only$Longitude %>% head(1)
-#create test latitude vector to check if function works
-test_lat <- dms_only$Latitude %>% head(1)
-
-#see if function correctly converts longitude, it DOESNT WORK!
-long_works_test <- lapply(test_long, fix_longitude)
-print(test_long)
-print(long_works_test)
-
-#see if function correcty converts latitude, it WORKS!
-lat_works_test <- lapply(test_lat, fix_latitude)
-print(lat_works_test)
-print(test_lat)
-
-##attempt to build own DMS to DD function to see if it resolves incorrect conversion issue when using 
-##sp::char2dms()
-dms2dd <- function(dms_coord) {
-  
-}
-
-## try to understand how to split strings, need to do this to create the deg, min, sec variables for
-## dms2dd function
-foo <- "35d14'4.322\"N"
-deg <- str_split(foo, 'd')[[1]][1]
-str_split(foo, ".*d")
-
-l 
-
-
-
-############LEFT OFF HERE: NEXT TIME - APPLY GSUB PATTERNS TO ENTIRE LATITUDE COLUMN, 
-##LONGITUDE (BUT W NOT N)
-#MAP COLUMNS IN DMS DATAFRAME TO FUNCTION, REPLACE THEM WITH DD, MERGE DATAFRAME WITH DD DATAFRAME
-
-
-as.numeric(test2)
-#convert dataframe to sf spatial dataframe object
-dd_only_sf <- st_as_sf(dd_only, coords = c('Longitude', 'Latitude'))
-dd_only_sf #inspect min, max lat longs to see if they make sense
-
-dd_only_utm <- dd_only_sf %>%
-  st_set_crs(4326) %>% #assume lat long to be WGS84
-  st_transform(crs = 32611) #project data to WGS84 UTM zone 11N
-dd_only_utm
-
-do.call(st_geometry(dd_only_utm), dd_only_utm$Genus_species) %>% as_tibble() %>% setNames(c('east', 'north'))
-test_df <- as.data.frame(dd_only_utm)
-test_df$geometry
-###extract northing, easting from geom column?
-test <- do.call(rbind, st_geometry(dd_only_utm)) %>% 
-  as_tibble(.name_repair = "minimal") %>% setNames(c("east","north"))
-test
-
-
-#############CLEAN SPATIAL DATA END
-
-#view data
-ggplot2::ggplot(spatial_bound, aes(x = Longitude, y = Latitude)) + geom_point() + 
-  theme(
-    axis.text.x = element_blank(),
-    axis.text.y = element_blank())
-
-
-######summary statistics 
-
-#find number of observations in df
-df_len <- nrow(my_data)
-
-#check number of observations that have lat/long
-num_latlong <- sum(!is.na(my_data$Latitude))
-#sum(is.na(my_data$Longitude)) # check to see if number that has long matches number that has lat
-
-num_utm <- sum(!is.na(my_data$Easting))# check to see number of observations that have easting /northing
-#sum(is.na(my_data$Northing)) 
-
-prop_utm <- round((num_utm / df_len), 3)
-prop_latlong <- round((num_latlong / df_len), 3)
-
-paste0(prop_utm*100, '% of the data have northing / easting, while ', prop_latlong*100, ' % of the data have lat/long')
-
-                        
-#######atttempt to resolve unknown moss / licken issue
-
-example_moss <- c('gray moss', 'Moss', 'Bright Green Moss', 'dried moss', 'moss', 'gray-brown moss')
-example_moss
+write.csv(taxa_list_df, file = "data/clean/taxa_list.csv", row.names = FALSE) # change path relevant to your directory organization
+write.table(taxa_list_df, file = "data/clean/taxa_list.txt", sep = " ", col.names = FALSE)
 
 
